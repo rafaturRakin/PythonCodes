f71a88f9e53321a1363eea67f0eb1067e2042591
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-09-05 22:26:04 +0600

    First program in python

---
+++ Basic/First.py
@@ -0,0 +1,13 @@
+"""
+#Author     : Rakin
+#Date       : 05-09-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Program    : First Program
+       CSE, 2018, HSTU
+"""
+
+print(10)
+print(12.45)
+print("Rakin")
+print(True)
