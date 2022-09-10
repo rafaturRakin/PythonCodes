b2d0e8228021712ad27b3f13563bbcdc93c19a55
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-09-10 22:48:07 +0600

    Comments and Escape Characters

---
+++ Basic/CommentsAndEscapCharacters.py
@@ -0,0 +1,20 @@
+"""
+#Author     : Rakin
+#Date       : 10-09-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Program    : Comments And Escape Characters Program
+       CSE, 2018, HSTU
+"""
+
+# Single Line Comment
+print("Single Line Comment -> #")
+
+# Multi-line Comment
+print('Multiple Line Comment -> """ """')
+
+# Escape Characters
+print("\nNew Line -> '\\n'")
+print("Tab -> '\\t'")
+print("Double Quotation -> '\\\"'")
+print("Single Quotation -> '\\\''")
