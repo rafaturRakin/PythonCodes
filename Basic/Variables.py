7ddf079a3e47eb4388bca78d363fcb65bb4a3f16
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-09-21 09:04:25 +0600

    Variables and DataType

---
+++ Basic/Variables.py
@@ -0,0 +1,18 @@
+"""
+#Author     : Rakin
+#Date       : 10-09-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Program    : Comments And Escape Characters Program
+       CSE, 2018, HSTU
+"""
+
+integerVariable = 12
+doubleVariable = 3.1615
+booleanVariable = True
+stringVariable = "Python"
+
+print("Integer = ", integerVariable)
+print("Double = ", doubleVariable)
+print("Boolean = ", booleanVariable)
+print("String = " + stringVariable)
