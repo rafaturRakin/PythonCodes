286926c0615c091aec22cf745dd18c1d8d4e714c
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-03 21:23:16 +0600

    Numerical Operators

---
+++ Basic/Example01.py
@@ -0,0 +1,22 @@
+"""
+#Author     : Rakin
+#Date       : 03-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Program    : Numerical Operators
+       CSE, 2018, HSTU
+"""
+
+first = 2
+second = 10
+
+# basic operators
+print(f"{first} + {second} = {first + second}")
+print(f"{first} - {second} = {first - second}")
+print(f"{first} * {second} = {first * second}")
+print(f"{first} / {second} = {first / second}")
+print(f"{first} % {second} = {first % second}")
+
+# Python special
+print(f"{first} // {second} = {first // second}")
+print(f"{first} ** {second} = {first ** second}")
