da9789ea16b12eb11bb2b7425494ff6463cc5c1c
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 19:01:18 +0600

    Printing Pattern - 02

---
+++ Basic/Exercise14.py
@@ -0,0 +1,19 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Basic exercise for beginners
+#Exercise   : 14
+       CSE, 2018, HSTU
+"""
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+for x in range(5, 0, -1):
+    print('* ' * x)
