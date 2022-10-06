ce4ac691b8498e0a33ab901fe8b0030c76291897
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 19:06:41 +0600

    IO - Formatting floats using print( )

---
+++ Basic/Exercise19.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Input and Output Exercise
+#Exercise   : 4
+       CSE, 2018, HSTU
+"""
+
+# Display float number with 2 decimal places using print()
+
+number = 458.541315
+print("%.2f" % number)
