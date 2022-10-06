e23739563aab363c525b8cf2a2177dfc87f082ce
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 19:01:52 +0600

    Exponent

---
+++ Basic/Exercise15.py
@@ -0,0 +1,18 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Basic exercise for beginners
+#Exercise   : 15
+       CSE, 2018, HSTU
+"""
+
+
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+def exponent(base, exp):
+    return base ** exp
+
+
+print(exponent(2, 5))
