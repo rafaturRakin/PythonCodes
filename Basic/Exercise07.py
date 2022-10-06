d21478c6f80251735d96ea2e4c2d46ff5b742346
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 18:54:01 +0600

    how many times substring occurs

---
+++ Basic/Exercise07.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Basic exercise for beginners
+#Exercise   : 07
+       CSE, 2018, HSTU
+"""
+
+
+# Write a program to find how many times substring “Emma” appears in the given string.
+str_x = "Emma is good developer. Emma is a writer"
+
+print(f"Emma appeared {str_x.count('Emma')} times")
