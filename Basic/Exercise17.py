ed9689ecd7a794a131aeee2ab4ff82b631a08ba9
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 19:05:03 +0600

    IO - Use of 'sep'

---
+++ Basic/Exercise17.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Input and Output Exercise
+#Exercise   : 2
+       CSE, 2018, HSTU
+"""
+
+# Use the print() function to format the given words in the mentioned format.
+# Display the ** separator between each string.
+
+print('Name', 'Is', 'James', sep='**')
