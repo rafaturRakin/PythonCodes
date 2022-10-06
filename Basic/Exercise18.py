f92434821ab93cb190971cd7aadbae58ebabed3a
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 19:05:47 +0600

    IO - Printing octal number using print( )

---
+++ Basic/Exercise18.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Input and Output Exercise
+#Exercise   : 3
+       CSE, 2018, HSTU
+"""
+
+# Convert Decimal number to octal using print() output formatting
+
+number = 12
+print("%o" % number)
