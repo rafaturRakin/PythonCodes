dabbfc4a7ff9f0c2f55ae0d3d3cf247d4ab043f7
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-06 19:03:56 +0600

    Multiplication

---
+++ Basic/Exercise16.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 06-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Input and Output Exercise
+#Exercise   : 1
+       CSE, 2018, HSTU
+"""
+
+# Write a program to accept two numbers from the user and calculate multiplication
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+print(f"product = {number1 * number2}")
