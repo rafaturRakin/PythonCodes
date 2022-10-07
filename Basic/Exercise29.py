90f0d72ad15d48070de3d0ba2e5306b829d0c994
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-07 19:37:23 +0600

    Loop - Multiplication table of n

---
+++ Basic/Exercise29.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 07-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Loop Exercise
+#Exercise   : 04
+       CSE, 2018, HSTU
+"""
+
+# Write a program to print multiplication table of a given number
+
+number = int(input("Enter the number : "))
+for i in range(1, 11):
+    print(number * i)
