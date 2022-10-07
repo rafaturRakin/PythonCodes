fc30b02bf5f3b7ea49dda8e44d5d18a5a7b213f4
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-07 19:41:32 +0600

    Loop - For with Else block

---
+++ Basic/Exercise35.py
@@ -0,0 +1,17 @@
+"""
+#Author     : Rakin
+#Date       : 07-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Loop Exercise
+#Exercise   : 10
+       CSE, 2018, HSTU
+"""
+
+# Use else block to display a message “Done” after successful execution of for loop
+
+for num in range(5):
+    print(num)
+else:
+    print("Done!")
