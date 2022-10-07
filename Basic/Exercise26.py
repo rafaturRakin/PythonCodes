41dd785fc3ba4dea5ed817808d2f13165caa8e89
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-07 19:35:12 +0600

    Loop - First 10 natural numbers

---
+++ Basic/Exercise26.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 07-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Loop Exercise
+#Exercise   : 01
+       CSE, 2018, HSTU
+"""
+
+# Print First 10 natural numbers using while loop
+number = 1
+while number <= 10:
+    print(number)
+    number += 1
