437517e02d6cc545f49774d3c6893f5bdc744e7a
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-07 19:40:29 +0600

    Loop - Printing -10 to -1

---
+++ Basic/Exercise34.py
@@ -0,0 +1,14 @@
+"""
+#Author     : Rakin
+#Date       : 07-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Loop Exercise
+#Exercise   : 09
+       CSE, 2018, HSTU
+"""
+
+# Display numbers from -10 to -1 using for loop
+for num in range(-10, 0, 1):
+    print(num)
