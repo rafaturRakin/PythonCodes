e51ac4974358d70b6ea65e7842631b229c6d83c5
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-08 17:18:57 +0600

    Function - Write a Function

---
+++ Basic/Exercise44.py
@@ -0,0 +1,19 @@
+"""
+#Author     : Rakin
+#Date       : 08-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Function Exercise
+#Exercise   : 01
+       CSE, 2018, HSTU
+"""
+
+
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+def introduction(name, age):
+    print(f"Hello {name}, you are {age} years old")
+
+
+introduction("Rakin", 22)
+introduction("John", 12)
