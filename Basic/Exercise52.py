e0987db0c423acb0fcd79b2c94150803dc18cb7d
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-08 17:24:39 +0600

    Function - maximum element of a list using max()

---
+++ Basic/Exercise52.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 08-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Function Exercise
+#Exercise   : 09
+       CSE, 2018, HSTU
+"""
+
+
+#  Find the largest item from a given list
+x = [4, 6, 8, 24, 12, 2]
+print(max(x))
