52373fd7389da47ae36e38b9499fc6c9abfe7f87
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-08 17:23:56 +0600

    Function - create list using list()

---
+++ Basic/Exercise51.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 08-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Function Exercise
+#Exercise   : 08
+       CSE, 2018, HSTU
+"""
+
+
+# Generate a Python list of all the even numbers between 4 and 30
+
+print(list(range(4, 31, 2)))
