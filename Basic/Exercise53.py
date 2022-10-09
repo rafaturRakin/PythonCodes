a657fcd417554970f372e5ee2d5ef5b866b1642f
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-09 19:54:18 +0600

    String -

---
+++ Basic/Exercise53.py
@@ -0,0 +1,17 @@
+"""
+#Author     : Rakin
+#Date       : 09-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : String Exercise
+#Exercise   : 01 A
+       CSE, 2018, HSTU
+"""
+
+
+# Create a string made of the first, middle and last character
+str1 = "James"
+result = str1[0] + str1[len(str1) // 2] + str1[-1]
+
+print(result)
