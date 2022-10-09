52d663255b1fcd12338f5401f5bf4e8042e62a17
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-09 19:57:36 +0600

    String - Middle 3 characters

---
+++ Basic/Exercise54.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 09-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : String Exercise
+#Exercise   : 01 B
+       CSE, 2018, HSTU
+"""
+
+
+# Create a string made of the middle three characters
+str1 = "JaSonAy"
+middle = len(str1) // 2
+print(str1[middle-1:middle+2])
