da4b8a6440e4574e1d8ee763332e819be71d3b46
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-10 09:51:48 +0600

    String - Reverse a string

---
+++ Basic/Exercise64.py
@@ -0,0 +1,18 @@
+"""
+#Author     : Rakin
+#Date       : 10-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : String Exercise
+#Exercise   : 11
+       CSE, 2018, HSTU
+"""
+
+
+# Reverse a given string
+string = "Python"
+reverse_string = string[::-1]
+
+print(f"Original string: '{string}' and Reversed string: '{reverse_string}'")
+# print("".join(reversed(string)))
