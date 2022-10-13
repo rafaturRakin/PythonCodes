f9d796b7e27486f42f1d121d3074dd04b3e993bc
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-13 19:29:26 +0600

    Dictionary - Printing a keys value

---
+++ Basic/Exercise94.py
@@ -0,0 +1,27 @@
+"""
+#Author     : Rakin
+#Date       : 13-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Dictionary Exercise
+#Exercise   : 03
+       CSE, 2018, HSTU
+"""
+
+
+# Print the value of key ‘history’ from the below dict
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+value_of_history = sampleDict["class"]["student"]["marks"]["history"]
+print(value_of_history)
