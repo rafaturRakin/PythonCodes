dec7d4ba9091b6e39bfa8b2d1353b929bb343a4f
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-13 19:33:04 +0600

    Dictionary - Checking existence of a value

---
+++ Basic/Exercise98.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 13-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Dictionary Exercise
+#Exercise   : 07
+       CSE, 2018, HSTU
+"""
+
+# Check if a value exists in a dictionary
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+dictionaryValues = sample_dict.values()
+
+print("200 present in the dictionary") if 200 in dictionaryValues else print("200 is not present")
