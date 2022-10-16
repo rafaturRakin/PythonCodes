17a360d07f9f0743a41e8c0eac702499d4ebaa34
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-16 21:07:18 +0600

    Set - Add a list of elements to a set

---
+++ Basic/Exercise102.py
@@ -0,0 +1,19 @@
+"""
+#Author     : Rakin
+#Date       : 16-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Set Exercise
+#Exercise   : 01
+       CSE, 2018, HSTU
+"""
+
+# Add a list of elements to a set
+
+sample_set = {"Yellow", "Orange", "Black"}
+sample_list = ["Blue", "Green", "Red"]
+
+sample_set.update(sample_list)
+
+print(sample_set)
