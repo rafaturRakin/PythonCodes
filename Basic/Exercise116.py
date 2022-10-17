32e8fb951c80af8c7c03729ce838a31cbcedf8e3
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:40:19 +0600

    Tuple - Copy specific elements from one tuple to a new tuple

---
+++ Basic/Exercise116.py
@@ -0,0 +1,18 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 06
+       CSE, 2018, HSTU
+"""
+
+# Copy specific elements from one tuple to a new tuple
+
+tuple1 = (11, 22, 33, 44, 55, 66)
+
+tuple2 = (tuple1[3:5])
+
+print(tuple1, tuple2)
