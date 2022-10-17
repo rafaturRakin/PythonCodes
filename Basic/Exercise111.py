632df5f555112c23fb6706eb576ae653ca1c553b
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:37:23 +0600

    Tuple - Reverse the tuple

---
+++ Basic/Exercise111.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 01
+       CSE, 2018, HSTU
+"""
+
+# Reverse the tuple
+tuple1 = (10, 20, 30, 40, 50)
+
+print(tuple(reversed(tuple1)))
+print(tuple1[::-1])
