73f29a862dbffd6085b3aa9dbebe6d3571ff8d93
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:39:36 +0600

    Tuple - Swap two tuples in Python

---
+++ Basic/Exercise115.py
@@ -0,0 +1,20 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 05
+       CSE, 2018, HSTU
+"""
+
+# Swap two tuples in Python
+tuple1 = (11, 22)
+tuple2 = (99, 88)
+
+print(tuple1, tuple2)
+
+tuple1, tuple2 = tuple2, tuple1
+
+print(tuple1, tuple2)
