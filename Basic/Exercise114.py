9aa7c80f377ad175ce7a17328a2dacc77de17fc9
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:39:06 +0600

    Tuple - Unpack the tuple into 4 variables

---
+++ Basic/Exercise114.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 04
+       CSE, 2018, HSTU
+"""
+
+# Unpack the tuple into 4 variables
+tuple1 = (10, 20, 30, 40)
+a, b, c, d = tuple1
+
+print(a, b, c, d)
