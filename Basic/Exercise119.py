1f98734d7df90c75fa262abf4c012c01942f7dbe
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:44:07 +0600

    Tuple - Counts the number of occurrences of item 50 from a tuple

---
+++ Basic/Exercise119.py
@@ -0,0 +1,16 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 119
+       CSE, 2018, HSTU
+"""
+
+# Counts the number of occurrences of item 50 from a tuple
+
+tuple1 = (50, 10, 60, 70, 50)
+
+print(tuple1.count(50))
