5336be9667ada30cf579f46887b0e4d50d01c3df
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:38:32 +0600

    Tuple - Create a tuple with single item 50

---
+++ Basic/Exercise113.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 03
+       CSE, 2018, HSTU
+"""
+
+# Create a tuple with single item 50
+
+myTuple = (50, )
+print(myTuple, type(myTuple))
