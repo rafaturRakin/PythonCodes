83b9a8b29be890d4ead61232d508aaae48a7130b
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-10-17 20:37:56 +0600

    Tuple - Access value 20 from the tuple

---
+++ Basic/Exercise112.py
@@ -0,0 +1,15 @@
+"""
+#Author     : Rakin
+#Date       : 17-10-2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+#Website    : Pynative
+#Topic      : Tuple Exercise
+#Exercise   : 02
+       CSE, 2018, HSTU
+"""
+
+# Access value 20 from the tuple
+
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+print(tuple1[1][1])
